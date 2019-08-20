d013ac02cd61ace80eaa6e4076e6842100ee4d9c
Sergio Barriel <sergiobarriel@gmail.com>
2019-08-20 23:41:03 +0200

    Added tests for energy

---
+++ src/PlanetExploration/Domain.Entities.Tests/EnergyShould.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Domain.Entities.Tests
+{
+    public class EnergyShould
+    {
+        [Fact]
+        public void energy_has_energy()
+        {
+            var energy = Energy.Create(100).Build();
+
+            Assert.True(energy.HasEnergy());
+        }
+
+        [Fact]
+        public void energy_discharge_works_correctly()
+        {
+            var energy = Energy.Create(100).Build();
+
+            energy.Discharge(10);
+
+            Assert.True(energy.GetLoad() == 90);
+        }
+
+        [Fact]
+        public void energy_charge_works_correctly()
+        {
+            var energy = Energy.Create(100).Build();
+
+            energy.Discharge(10);
+            Assert.True(energy.GetLoad() == 90);
+
+            energy.Charge();
+            Assert.True(energy.GetLoad() == 100);
+        }
+    }
+}
